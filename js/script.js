a90dc4120f189095b9af79df001c6e0f96202aca
Kinga Milaszkiewicz <kinga.milaszkiewicz@outlook.com>
2020-01-17 22:23:29 +0100

    Add game functionality

--- js/script.js
+++ js/script.js
@@ -1,38 +1,12 @@
 var computerMove, playerMove, randomNumber;
 
 playerInput = prompt("Choose your turn! 1: stone, 2: paper, 3: scissors.");
-
-console.log("Answer is: " + playerInput);
-
-if (playerInput == "1") {
-  playerMove = "stone";
-} else if (playerInput == "2") {
-  playerMove = "paper";
-} else if (playerInput == "3") {
-  playerMove = "scissors";
-} else {
-  playerMove = "unknown input";
-}
-
-printMessage("Your move: " + playerMove);
-
+console.log ("Player choice is " + playerInput);
+playerMove = getMoveName(playerInput);
+console.log("Player move is " + playerMove);
 randomNumber = getRndInteger(1, 3);
+console.log("Random number is " + randomNumber);
+computerMove = getMoveName(randomNumber);
+console.log("Computer move is " + computerMove);
 
-if (randomNumber == "1") {
-  computerMove = "stone";
-} else if (randomNumber == "2") {
-  computerMove = "paper";
-} else {
-  computerMove = "scissors";
-}
-
-printMessage("Computer move: " + computerMove);
-
-
-// var randomNumber, anotherNumber;
-//
-// randomNumber = getRndInteger(1, 3);
-// printMessage("Chosen number: " + randomNumber);
-//
-// anotherNumber = getRndInteger(11, 19);
-// printMessage("anotherNumber: " + anotherNumber);
+displayResult(playerMove, computerMove);
