79994f0867114446034acf0156b00a157ebdffa8
Kinga Milaszkiewicz <kinga.milaszkiewicz@outlook.com>
2020-01-21 23:19:12 +0100

    Fix bug

--- js/script.js
+++ js/script.js
@@ -13,6 +13,15 @@ buttonScissors.addEventListener("click", function(){ buttonClicked("3");});
 
 function buttonClicked(argButtonName) {
 	console.log(argButtonName + " has been clicked.");
+
+	playerMove = getMoveName(argButtonName);
+	console.log("Player move is " + playerMove);
+	randomNumber = getRndInteger(1, 3);
+	console.log("Random number is " + randomNumber);
+	computerMove = getMoveName(randomNumber);
+	console.log("Computer move is " + computerMove);
+
+	displayResult(playerMove, computerMove);
 }
 
  //////////////////////////////////////////////////////////////////////////////
@@ -20,14 +29,14 @@ function buttonClicked(argButtonName) {
 function getMoveName(argMoveId) {
 	console.log("Called getMoveName function with argument: " + argMoveId);
 	if (argMoveId == 1) {
-		return "stone";
+		return "rock";
 	} else if (argMoveId == 2) {
 		return "paper";
 	} else if (argMoveId == 3) {
 		return "scissors";
 	} else {
 		printMessage("I don't know a move with id " + argMoveId + ". I am assuming you meant 'stone'.");
-		return "stone";
+		return "rock";
 	}
 }
 
@@ -35,22 +44,12 @@ function displayResult(argPlayerMove, argComputerMove) {
 	console.log("Called getMoveName function with arguments: " + argPlayerMove + ", " + argComputerMove);
 	if (argPlayerMove == argComputerMove) {
 		printMessage("Draw!")
-	} else if (argPlayerMove == "stone" && argComputerMove == "scissors" || argPlayerMove == "paper" && argComputerMove == "stone" || argPlayerMove == "scissors" && argComputerMove == "paper") {
+	} else if (argPlayerMove == "rock" && argComputerMove == "scissors" || argPlayerMove == "paper" && argComputerMove == "rock" || argPlayerMove == "scissors" && argComputerMove == "paper") {
 		printMessage("You won!");
 	} else {
-		printMessage("You lost :(")
+		printMessage("You lost :(");
 	}
 	printMessage("I played " + argComputerMove + " and you played " + argPlayerMove + ".");
 }
 
  //////////////////////////////////////////////////////////////////////////////
-
-playerMove = getMoveName(argButtonName);
-console.log("Player move is " + playerMove);
-randomNumber = getRndInteger(1, 3);
-console.log("Random number is " + randomNumber);
-computerMove = getMoveName(randomNumber);
-console.log("Computer move is " + computerMove);
-
-
-displayResult(playerMove, computerMove);
